0198bf275fff3caf1fbca4938485262777233155
Roshan Dash <roshan1337d@gmail.com>
2023-04-16 12:47:15 +0530

    api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+// Api base url
 // window.APIROOT = "http://127.0.0.1:4950/";
 window.APIROOT = "https://healthy-home-meals-api.roshan.cyou/";
 
